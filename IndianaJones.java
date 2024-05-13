932194092972bda86808d63e8c98ec3fc9c143a1
elisechldn <elise.chaldjian@gmail.com>
2024-05-13 22:00:41 +0200

    Exercice pour appréhender Java et son typage de variables. Réalisé dans le cadre d'une formation Java à la Wild Code School.

---
+++ IndianaJones.java
@@ -0,0 +1,13 @@
+public class IndianaJones { 
+    public static void main(String[] args) {
+        String filmName = "Indiana Jones and the Last Crusade";
+        boolean alreadyWatched = true;
+        int releaseYear = 1989;
+        float rating = 8.2f;
+
+        System.out.println(filmName);
+        System.out.println(alreadyWatched);
+        System.out.println(releaseYear);
+        System.out.println(rating);
+    }
+}
---
+++ indianaJones.java
@@ -0,0 +1,13 @@
+public class IndianaJones { 
+    public static void main(String[] args) {
+        String filmName = "Indiana Jones and the Last Crusade";
+        boolean alreadyWatched = true;
+        int releaseDate = 1989;
+        float rating = 8.2f;
+
+        System.out.println(filmName);
+        System.out.println(alreadyWatched);
+        System.out.println(releaseDate);
+        System.out.println(rating);
+    }
+}
